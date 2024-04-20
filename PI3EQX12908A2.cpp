72f03186f8c98b4315b45eb0f113950f564e96bf
SMotlaq <pilot.motlaq@gmail.com>
2024-04-20 14:57:07 +0330

    first commit

---
+++ PI3EQX12908A2.cpp
@@ -0,0 +1,782 @@
+#include "PI3EQX12908A2.h"
+#include "Arduino.h"
+
+PI3EQX12908::init(uint8_t i2c_addr){
+  _I2C_ADDR = i2c_addr;
+
+  _REGS[0]  = "SIGNAL DETECT";
+  _REGS[1]  = "    RX DETECT";
+  _REGS[2]  = "   POWER DOWN";
+  _REGS[3]  = "   CHANNEL A0";
+  _REGS[4]  = "   CHANNEL A1";
+  _REGS[5]  = "   CHANNEL A2";
+  _REGS[6]  = "   CHANNEL A3";
+  _REGS[7]  = "   CHANNEL B0";
+  _REGS[8]  = "   CHANNEL B1";
+  _REGS[9]  = "   CHANNEL B2";
+  _REGS[10] = "   CHANNEL B3";
+  _REGS[11] = "  SIG DET CFG";
+  _REGS[12] = "   RX DET CFG";
+  _REGS[13] = "  SIG DET THR";
+  _REGS[14] = "    14th BYTE";
+  _REGS[15] = "    15th BYTE";
+}
+
+// 0 - Signal Detect
+uint8_t PI3EQX12908::getSignalDetect(){
+  return _read_reg(SIGNAL_DETECT_REG);
+}
+
+uint8_t PI3EQX12908::getSignalDetect_A(){
+  return _read_reg(SIGNAL_DETECT_REG) >> 4;
+}
+
+uint8_t PI3EQX12908::getSignalDetect_A(uint8_t index){
+  return _read_reg(SIGNAL_DETECT_REG) & (1 << (index + 4));
+}
+
+uint8_t PI3EQX12908::getSignalDetect_B(){
+  return _read_reg(SIGNAL_DETECT_REG) & 0x0F;
+}
+
+uint8_t PI3EQX12908::getSignalDetect_B(uint8_t index){
+  return _read_reg(SIGNAL_DETECT_REG) & (1 << index);
+}
+
+// 1 - RX Detect
+uint8_t PI3EQX12908::getRxDetect(){
+  return _read_reg(RX_DETECT_REG);
+}
+
+uint8_t PI3EQX12908::getRxDetect_A(){
+  return _read_reg(RX_DETECT_REG) >> 4;
+}
+
+uint8_t PI3EQX12908::getRxDetect_A(uint8_t index){
+  return _read_reg(RX_DETECT_REG) & (1 << (index + 4));
+}
+
+uint8_t PI3EQX12908::getRxDetect_B(){
+  return _read_reg(RX_DETECT_REG) & 0x0F;
+}
+
+uint8_t PI3EQX12908::getRxDetect_B(uint8_t index){
+  return _read_reg(RX_DETECT_REG) & (1 << index);
+}
+
+// 2 - Power Down
+uint8_t PI3EQX12908::getPowerDown(){
+  return _read_reg(POWER_DOWN_REG);
+}
+
+uint8_t PI3EQX12908::getPowerDown_A(){
+  return _read_reg(POWER_DOWN_REG) >> 4;
+}
+
+uint8_t PI3EQX12908::getPowerDown_A(uint8_t index){
+  return _read_reg(POWER_DOWN_REG) & (1 << (index + 4));
+}
+
+uint8_t PI3EQX12908::getPowerDown_B(){
+  return _read_reg(POWER_DOWN_REG) & 0x0F;
+}
+
+uint8_t PI3EQX12908::getPowerDown_B(uint8_t index){
+  return _read_reg(POWER_DOWN_REG) & (1 << index);
+}
+
+void PI3EQX12908::setPowerDown(uint8_t isDown){
+  _write_reg(POWER_DOWN_REG, isDown ? 0xFF : 0x00);
+}
+
+void PI3EQX12908::setPowerDown_A(uint8_t isDown){
+  uint8_t val = getPowerDown();
+  if(isDown)
+    val |= (uint8_t)0xF0;
+  else
+    val &= (uint8_t)0x0F;
+  _write_reg(POWER_DOWN_REG, val);
+}
+
+void PI3EQX12908::setPowerDown_A(uint8_t index, uint8_t isDown){
+  uint8_t val = getPowerDown();
+  if(isDown)
+    val |= (1 << (index + 4));
+  else
+    val &= ~(1 << (index + 4));
+  _write_reg(POWER_DOWN_REG, val);
+}
+
+void PI3EQX12908::setPowerDown_B(uint8_t isDown){
+  uint8_t val = getPowerDown();
+  if(isDown)
+    val |= (uint8_t)0x0F;
+  else
+    val &= (uint8_t)0xF0;
+  _write_reg(POWER_DOWN_REG, val);
+}
+
+void PI3EQX12908::setPowerDown_B(uint8_t index, uint8_t isDown){
+  uint8_t val = getPowerDown();
+  if(isDown)
+    val |= (1 << index);
+  else
+    val &= ~(1 << index);
+  _write_reg(POWER_DOWN_REG, val);
+}
+
+// 3 - A0 Config
+uint8_t PI3EQX12908::getConfig_A0(){
+  return _read_reg(CONFIG_A0_REG);
+}
+
+uint8_t PI3EQX12908::getEQ_A0(){
+  return _read_reg(CONFIG_A0_REG) >> EQ_SHIFT;
+}
+
+uint8_t PI3EQX12908::getFlatGain_A0(){
+  return (_read_reg(CONFIG_A0_REG) & 0x0F) >> FG_SHIFT;
+}
+
+uint8_t PI3EQX12908::getSW_A0(){
+  return _read_reg(CONFIG_A0_REG) & 0x01;
+}
+
+void PI3EQX12908::setConfig_A0(uint8_t config){
+  _write_reg(CONFIG_A0_REG, config);
+}
+
+void PI3EQX12908::setEQ_A0(uint8_t EQ){
+  uint8_t val = getConfig_A0();
+  val &= 0x0F;
+  val |= (EQ & 0x0F) << EQ_SHIFT;
+  _write_reg(CONFIG_A0_REG, val);
+}
+
+void PI3EQX12908::setFlatGain_A0(uint8_t flat_gain){
+  uint8_t val = getConfig_A0();
+  val &= 0xF3;
+  val |= (flat_gain & 0x03) << FG_SHIFT;
+  _write_reg(CONFIG_A0_REG, val);
+}
+
+void PI3EQX12908::setSW_A0(uint8_t swing){
+  uint8_t val = getConfig_A0();
+  val &= 0xFE;
+  val |= swing & 0x01;
+  _write_reg(CONFIG_A0_REG, val);
+}
+
+// 4 - A1 Config
+uint8_t PI3EQX12908::getConfig_A1(){
+  return _read_reg(CONFIG_A1_REG);
+}
+
+uint8_t PI3EQX12908::getEQ_A1(){
+  return _read_reg(CONFIG_A1_REG) >> EQ_SHIFT;
+}
+
+uint8_t PI3EQX12908::getFlatGain_A1(){
+  return (_read_reg(CONFIG_A1_REG) & 0x0F) >> FG_SHIFT;
+}
+
+uint8_t PI3EQX12908::getSW_A1(){
+  return _read_reg(CONFIG_A1_REG) & 0x01;
+}
+
+void PI3EQX12908::setConfig_A1(uint8_t config){
+  _write_reg(CONFIG_A1_REG, config);
+}
+
+void PI3EQX12908::setEQ_A1(uint8_t EQ){
+  uint8_t val = getConfig_A1();
+  val &= 0x0F;
+  val |= (EQ & 0x0F) << EQ_SHIFT;
+  _write_reg(CONFIG_A1_REG, val);
+}
+
+void PI3EQX12908::setFlatGain_A1(uint8_t flat_gain){
+  uint8_t val = getConfig_A1();
+  val &= 0xF3;
+  val |= (flat_gain & 0x03) << FG_SHIFT;
+  _write_reg(CONFIG_A1_REG, val);
+}
+
+void PI3EQX12908::setSW_A1(uint8_t swing){
+  uint8_t val = getConfig_A1();
+  val &= 0xFE;
+  val |= swing & 0x01;
+  _write_reg(CONFIG_A1_REG, val);
+}
+
+// 5 - A2 Config
+uint8_t PI3EQX12908::getConfig_A2(){
+  return _read_reg(CONFIG_A2_REG);
+}
+
+uint8_t PI3EQX12908::getEQ_A2(){
+  return _read_reg(CONFIG_A2_REG) >> EQ_SHIFT;
+}
+
+uint8_t PI3EQX12908::getFlatGain_A2(){
+  return (_read_reg(CONFIG_A2_REG) & 0x0F) >> FG_SHIFT;
+}
+
+uint8_t PI3EQX12908::getSW_A2(){
+  return _read_reg(CONFIG_A2_REG) & 0x01;
+}
+
+void PI3EQX12908::setConfig_A2(uint8_t config){
+  _write_reg(CONFIG_A2_REG, config);
+}
+
+void PI3EQX12908::setEQ_A2(uint8_t EQ){
+  uint8_t val = getConfig_A2();
+  val &= 0x0F;
+  val |= (EQ & 0x0F) << EQ_SHIFT;
+  _write_reg(CONFIG_A2_REG, val);
+}
+
+void PI3EQX12908::setFlatGain_A2(uint8_t flat_gain){
+  uint8_t val = getConfig_A2();
+  val &= 0xF3;
+  val |= (flat_gain & 0x03) << FG_SHIFT;
+  _write_reg(CONFIG_A2_REG, val);
+}
+
+void PI3EQX12908::setSW_A2(uint8_t swing){
+  uint8_t val = getConfig_A2();
+  val &= 0xFE;
+  val |= swing & 0x01;
+  _write_reg(CONFIG_A2_REG, val);
+}
+
+// 6 - A3 Config
+uint8_t PI3EQX12908::getConfig_A3(){
+  return _read_reg(CONFIG_A3_REG);
+}
+
+uint8_t PI3EQX12908::getEQ_A3(){
+  return _read_reg(CONFIG_A3_REG) >> EQ_SHIFT;
+}
+
+uint8_t PI3EQX12908::getFlatGain_A3(){
+  return (_read_reg(CONFIG_A3_REG) & 0x0F) >> FG_SHIFT;
+}
+
+uint8_t PI3EQX12908::getSW_A3(){
+  return _read_reg(CONFIG_A3_REG) & 0x01;
+}
+
+void PI3EQX12908::setConfig_A3(uint8_t config){
+  _write_reg(CONFIG_A3_REG, config);
+}
+
+void PI3EQX12908::setEQ_A3(uint8_t EQ){
+  uint8_t val = getConfig_A3();
+  val &= 0x0F;
+  val |= (EQ & 0x0F) << EQ_SHIFT;
+  _write_reg(CONFIG_A3_REG, val);
+}
+
+void PI3EQX12908::setFlatGain_A3(uint8_t flat_gain){
+  uint8_t val = getConfig_A3();
+  val &= 0xF3;
+  val |= (flat_gain & 0x03) << FG_SHIFT;
+  _write_reg(CONFIG_A3_REG, val);
+}
+
+void PI3EQX12908::setSW_A3(uint8_t swing){
+  uint8_t val = getConfig_A3();
+  val &= 0xFE;
+  val |= swing & 0x01;
+  _write_reg(CONFIG_A3_REG, val);
+}
+
+// 7 - B0 Config
+uint8_t PI3EQX12908::getConfig_B0(){
+  return _read_reg(CONFIG_B0_REG);
+}
+
+uint8_t PI3EQX12908::getEQ_B0(){
+  return _read_reg(CONFIG_B0_REG) >> EQ_SHIFT;
+}
+
+uint8_t PI3EQX12908::getFlatGain_B0(){
+  return (_read_reg(CONFIG_B0_REG) & 0x0F) >> FG_SHIFT;
+}
+
+uint8_t PI3EQX12908::getSW_B0(){
+  return _read_reg(CONFIG_B0_REG) & 0x01;
+}
+
+void PI3EQX12908::setConfig_B0(uint8_t config){
+  _write_reg(CONFIG_B0_REG, config);
+}
+
+void PI3EQX12908::setEQ_B0(uint8_t EQ){
+  uint8_t val = getConfig_B0();
+  val &= 0x0F;
+  val |= (EQ & 0x0F) << EQ_SHIFT;
+  _write_reg(CONFIG_B0_REG, val);
+}
+
+void PI3EQX12908::setFlatGain_B0(uint8_t flat_gain){
+  uint8_t val = getConfig_B0();
+  val &= 0xF3;
+  val |= (flat_gain & 0x03) << FG_SHIFT;
+  _write_reg(CONFIG_B0_REG, val);
+}
+
+void PI3EQX12908::setSW_B0(uint8_t swing){
+  uint8_t val = getConfig_B0();
+  val &= 0xFE;
+  val |= swing & 0x01;
+  _write_reg(CONFIG_B0_REG, val);
+}
+
+// 8 - B1 Config
+uint8_t PI3EQX12908::getConfig_B1(){
+  return _read_reg(CONFIG_B1_REG);
+}
+
+uint8_t PI3EQX12908::getEQ_B1(){
+  return _read_reg(CONFIG_B1_REG) >> EQ_SHIFT;
+}
+
+uint8_t PI3EQX12908::getFlatGain_B1(){
+  return (_read_reg(CONFIG_B1_REG) & 0x0F) >> FG_SHIFT;
+}
+
+uint8_t PI3EQX12908::getSW_B1(){
+  return _read_reg(CONFIG_B1_REG) & 0x01;
+}
+
+void PI3EQX12908::setConfig_B1(uint8_t config){
+  _write_reg(CONFIG_B1_REG, config);
+}
+
+void PI3EQX12908::setEQ_B1(uint8_t EQ){
+  uint8_t val = getConfig_B1();
+  val &= 0x0F;
+  val |= (EQ & 0x0F) << EQ_SHIFT;
+  _write_reg(CONFIG_B1_REG, val);
+}
+
+void PI3EQX12908::setFlatGain_B1(uint8_t flat_gain){
+  uint8_t val = getConfig_B1();
+  val &= 0xF3;
+  val |= (flat_gain & 0x03) << FG_SHIFT;
+  _write_reg(CONFIG_B1_REG, val);
+}
+
+void PI3EQX12908::setSW_B1(uint8_t swing){
+  uint8_t val = getConfig_B1();
+  val &= 0xFE;
+  val |= swing & 0x01;
+  _write_reg(CONFIG_B1_REG, val);
+}
+
+// 9 - B2 Config
+uint8_t PI3EQX12908::getConfig_B2(){
+  return _read_reg(CONFIG_B2_REG);
+}
+
+uint8_t PI3EQX12908::getEQ_B2(){
+  return _read_reg(CONFIG_B2_REG) >> EQ_SHIFT;
+}
+
+uint8_t PI3EQX12908::getFlatGain_B2(){
+  return (_read_reg(CONFIG_B2_REG) & 0x0F) >> FG_SHIFT;
+}
+
+uint8_t PI3EQX12908::getSW_B2(){
+  return _read_reg(CONFIG_B2_REG) & 0x01;
+}
+
+void PI3EQX12908::setConfig_B2(uint8_t config){
+  _write_reg(CONFIG_B2_REG, config);
+}
+
+void PI3EQX12908::setEQ_B2(uint8_t EQ){
+  uint8_t val = getConfig_B2();
+  val &= 0x0F;
+  val |= (EQ & 0x0F) << EQ_SHIFT;
+  _write_reg(CONFIG_B2_REG, val);
+}
+
+void PI3EQX12908::setFlatGain_B2(uint8_t flat_gain){
+  uint8_t val = getConfig_B2();
+  val &= 0xF3;
+  val |= (flat_gain & 0x03) << FG_SHIFT;
+  _write_reg(CONFIG_B2_REG, val);
+}
+
+void PI3EQX12908::setSW_B2(uint8_t swing){
+  uint8_t val = getConfig_B2();
+  val &= 0xFE;
+  val |= swing & 0x01;
+  _write_reg(CONFIG_B2_REG, val);
+}
+
+// 10 - B3 Config
+uint8_t PI3EQX12908::getConfig_B3(){
+  return _read_reg(CONFIG_B3_REG);
+}
+
+uint8_t PI3EQX12908::getEQ_B3(){
+  return _read_reg(CONFIG_B3_REG) >> EQ_SHIFT;
+}
+
+uint8_t PI3EQX12908::getFlatGain_B3(){
+  return (_read_reg(CONFIG_B3_REG) & 0x0F) >> FG_SHIFT;
+}
+
+uint8_t PI3EQX12908::getSW_B3(){
+  return _read_reg(CONFIG_B3_REG) & 0x01;
+}
+
+void PI3EQX12908::setConfig_B3(uint8_t config){
+  _write_reg(CONFIG_B3_REG, config);
+}
+
+void PI3EQX12908::setEQ_B3(uint8_t EQ){
+  uint8_t val = getConfig_B3();
+  val &= 0x0F;
+  val |= (EQ & 0x0F) << EQ_SHIFT;
+  _write_reg(CONFIG_B3_REG, val);
+}
+
+void PI3EQX12908::setFlatGain_B3(uint8_t flat_gain){
+  uint8_t val = getConfig_B3();
+  val &= 0xF3;
+  val |= (flat_gain & 0x03) << FG_SHIFT;
+  _write_reg(CONFIG_B3_REG, val);
+}
+
+void PI3EQX12908::setSW_B3(uint8_t swing){
+  uint8_t val = getConfig_B3();
+  val &= 0xFE;
+  val |= swing & 0x01;
+  _write_reg(CONFIG_B3_REG, val);
+}
+
+// 11 - Signal Detect Config
+uint8_t PI3EQX12908::getSignalDetectConfig(){
+  return _read_reg(SIGNAL_DET_CFG_REG);
+}
+
+uint8_t PI3EQX12908::getSignalDetectConfig_A(){
+  return _read_reg(SIGNAL_DET_CFG_REG) >> 4;
+}
+
+uint8_t PI3EQX12908::getSignalDetectConfig_A(uint8_t index){
+  return _read_reg(SIGNAL_DET_CFG_REG) & (1 << (index + 4));
+}
+
+uint8_t PI3EQX12908::getSignalDetectConfig_B(){
+  return _read_reg(SIGNAL_DET_CFG_REG) & 0x0F;
+}
+
+uint8_t PI3EQX12908::getSignalDetectConfig_B(uint8_t index){
+  return _read_reg(SIGNAL_DET_CFG_REG) & (1 << index);
+}
+
+void PI3EQX12908::setSignalDetectConfig(uint8_t isDown){
+  _write_reg(SIGNAL_DET_CFG_REG, isDown ? 0xFF : 0x00);
+}
+
+void PI3EQX12908::setSignalDetectConfig_A(uint8_t isDown){
+  uint8_t val = getSignalDetectConfig();
+  if(isDown)
+    val |= 0xF0;
+  else
+    val &= 0x0F;
+  _write_reg(SIGNAL_DET_CFG_REG, val);
+}
+
+void PI3EQX12908::setSignalDetectConfig_A(uint8_t index, uint8_t isDown){
+  uint8_t val = getSignalDetectConfig();
+  if(isDown)
+    val |= (1 << (index + 4));
+  else
+    val &= ~(1 << (index + 4));
+  _write_reg(SIGNAL_DET_CFG_REG, val);
+}
+
+void PI3EQX12908::setSignalDetectConfig_B(uint8_t isDown){
+  uint8_t val = getSignalDetectConfig();
+  if(isDown)
+    val |= 0x0F;
+  else
+    val &= 0xF0;
+  _write_reg(SIGNAL_DET_CFG_REG, val);
+}
+void PI3EQX12908::setSignalDetectConfig_B(uint8_t index, uint8_t isDown){
+  uint8_t val = getSignalDetectConfig();
+  if(isDown)
+    val |= (1 << index);
+  else
+    val &= ~(1 << index);
+  _write_reg(SIGNAL_DET_CFG_REG, val);
+}
+
+// 12 - RX Detect Config
+uint8_t PI3EQX12908::getRxDetectConfig(){
+  return _read_reg(RX_DET_CFG_REG);
+}
+
+uint8_t PI3EQX12908::getRxDetectConfig_A(){
+  return _read_reg(RX_DET_CFG_REG) >> 4;
+}
+
+uint8_t PI3EQX12908::getRxDetectConfig_A(uint8_t index){
+  return _read_reg(RX_DET_CFG_REG) & (1 << (index + 4));
+}
+
+uint8_t PI3EQX12908::getRxDetectConfig_B(){
+  return _read_reg(RX_DET_CFG_REG) & 0x0F;
+}
+
+uint8_t PI3EQX12908::getRxDetectConfig_B(uint8_t index){
+  return _read_reg(RX_DET_CFG_REG) & (1 << index);
+}
+
+void PI3EQX12908::setRxDetectConfig(uint8_t isDown){
+  _write_reg(RX_DET_CFG_REG, isDown ? 0xFF : 0x00);
+}
+
+void PI3EQX12908::setRxDetectConfig_A(uint8_t isDown){
+  uint8_t val = getRxDetectConfig();
+  if(isDown)
+    val |= 0xF0;
+  else
+    val &= 0x0F;
+  _write_reg(RX_DET_CFG_REG, val);
+}
+
+void PI3EQX12908::setRxDetectConfig_A(uint8_t index, uint8_t isDown){
+  uint8_t val = getRxDetectConfig();
+  if(isDown)
+    val |= (1 << (index + 4));
+  else
+    val &= ~(1 << (index + 4));
+  _write_reg(RX_DET_CFG_REG, val);
+}
+
+void PI3EQX12908::setRxDetectConfig_B(uint8_t isDown){
+  uint8_t val = getRxDetectConfig();
+  if(isDown)
+    val |= 0x0F;
+  else
+    val &= 0xF0;
+  _write_reg(RX_DET_CFG_REG, val);
+}
+
+void PI3EQX12908::setRxDetectConfig_B(uint8_t index, uint8_t isDown){
+  uint8_t val = getRxDetectConfig();
+  if(isDown)
+    val |= (1 << index);
+  else
+    val &= ~(1 << index);
+  _write_reg(RX_DET_CFG_REG, val);
+}
+
+// 13 - Signal Detect Threshold
+uint8_t PI3EQX12908::getSDTConfig(){
+  return _read_reg(SIGNAL_DET_TH_REG);
+}
+
+uint8_t PI3EQX12908::setSDTConfig(uint8_t thresh){
+  uint8_t val = getSDTConfig();
+  val &= 0x03;
+  val |= (thresh & 0x03) << SDT_SHIFT;
+  _write_reg(SIGNAL_DET_TH_REG, val);
+}
+
+
+// Others
+void PI3EQX12908::setConfig_A(uint8_t config){
+  uint8_t data[4] = {config, config, config, config};
+  _burst_write(CONFIG_A0_REG, data, 4);
+}
+
+void PI3EQX12908::setConfig_A(uint8_t index, uint8_t config){
+  _write_reg(CONFIG_A_OFFSET + index, config);
+}
+
+void PI3EQX12908::setConfig_B(uint8_t config){
+  uint8_t data[4] = {config, config, config, config};
+  _burst_write(CONFIG_B0_REG, data, 4);
+}
+
+void PI3EQX12908::setConfig_B(uint8_t index, uint8_t config){
+  _write_reg(CONFIG_B_OFFSET + index, config);
+}
+
+void PI3EQX12908::setConfig(uint8_t config){
+  uint8_t data[8] = {config, config, config, config, config, config, config, config};
+  _burst_write(CONFIG_A0_REG, data, 8);
+}
+
+void PI3EQX12908::setEQ_A(uint8_t EQ){
+  uint8_t val[4];
+  _burst_read(CONFIG_A0_REG, val, 4);
+  for(uint8_t i=0; i<4; i++){
+    val[i] &= 0x0F;
+    val[i] |= (EQ & 0x0F) << EQ_SHIFT;
+  }
+  _burst_write(CONFIG_A0_REG, val, 4);
+}
+
+void PI3EQX12908::setEQ_B(uint8_t EQ){
+  uint8_t val[4];
+  _burst_read(CONFIG_B0_REG, val, 4);
+  for(uint8_t i=0; i<4; i++){
+    val[i] &= 0x0F;
+    val[i] |= (EQ & 0x0F) << EQ_SHIFT;
+  }
+  _burst_write(CONFIG_B0_REG, val, 4);
+}
+
+void PI3EQX12908::setEQ(uint8_t EQ){
+  //setEQ_A(EQ);
+  //setEQ_B(EQ);
+  //*/
+  uint8_t val[8];
+  _burst_read(CONFIG_A0_REG, val, 8);
+  for(uint8_t i=0; i<8; i++){
+    val[i] &= 0x0F;
+    val[i] |= (EQ & 0x0F) << EQ_SHIFT;
+  }
+  _burst_write(CONFIG_A0_REG, val, 8);
+  //*/
+}
+
+void PI3EQX12908::setFG_A(uint8_t flat_gain){
+  uint8_t val[4];
+  _burst_read(CONFIG_A0_REG, val, 4);
+  for(uint8_t i=0; i<4; i++){
+    val[i] &= 0xF3;
+    val[i] |= (flat_gain & 0x03) << FG_SHIFT;
+  }
+  _burst_write(CONFIG_A0_REG, val, 4);
+}
+
+void PI3EQX12908::setFG_B(uint8_t flat_gain){
+  uint8_t val[4];
+  _burst_read(CONFIG_B0_REG, val, 4);
+  for(uint8_t i=0; i<4; i++){
+    val[i] &= 0xF3;
+    val[i] |= (flat_gain & 0x03) << FG_SHIFT;
+  }
+  _burst_write(CONFIG_B0_REG, val, 4);
+}
+
+void PI3EQX12908::setFG(uint8_t flat_gain){
+  //setFG_A(flat_gain);
+  //setFG_B(flat_gain);
+  //*/
+  uint8_t val[8];
+  _burst_read(CONFIG_A0_REG, val, 8);
+  for(uint8_t i=0; i<8; i++){
+    val[i] &= 0xF3;
+    val[i] |= (flat_gain & 0x03) << FG_SHIFT;
+  }
+  _burst_write(CONFIG_A0_REG, val, 8);
+  //*/
+}
+
+void PI3EQX12908::setSW_A(uint8_t swing){
+  uint8_t val[4];
+  _burst_read(CONFIG_A0_REG, val, 4);
+  for(uint8_t i=0; i<4; i++){
+    val[i] &= 0xFE;
+    val[i] |= (swing & 0x01) << SW_SHIFT;
+  }
+  _burst_write(CONFIG_A0_REG, val, 4);
+}
+
+void PI3EQX12908::setSW_B(uint8_t swing){
+  uint8_t val[4];
+  _burst_read(CONFIG_B0_REG, val, 4);
+  for(uint8_t i=0; i<4; i++){
+    val[i] &= 0xFE;
+    val[i] |= (swing & 0x01) << SW_SHIFT;
+  }
+  _burst_write(CONFIG_B0_REG, val, 4);
+}
+
+void PI3EQX12908::setSW(uint8_t swing){
+  //setSW_A(swing);
+  //setSW_B(swing);
+  //*/
+  uint8_t val[8];
+  _burst_read(CONFIG_A0_REG, val, 8);
+  for(uint8_t i=0; i<8; i++){
+    val[i] &= 0xFE;
+    val[i] |= (swing & 0x01) << SW_SHIFT;
+  }
+  _burst_write(CONFIG_A0_REG, val, 8);
+  //*/
+}
+
+void PI3EQX12908::print_all(){
+  uint8_t data[16];
+  dump_all(data);
+  for(uint8_t i=0; i<16; i++){
+    Serial.print(_REGS[i]);
+    Serial.print(" = BIN: ");
+    Serial.print((data[i] >> 7) & 0x01);
+    Serial.print((data[i] >> 6) & 0x01);
+    Serial.print((data[i] >> 5) & 0x01);
+    Serial.print((data[i] >> 4) & 0x01);
+    Serial.print((data[i] >> 3) & 0x01);
+    Serial.print((data[i] >> 2) & 0x01);
+    Serial.print((data[i] >> 1) & 0x01);
+    Serial.print((data[i] >> 0) & 0x01);
+    Serial.print(" - HEX: 0x");
+    Serial.print(data[i] >> 4, HEX);
+    Serial.println(data[i] & 0x0F, HEX);
+  }
+}
+
+void PI3EQX12908::dump_all(uint8_t* data){
+  _burst_read(0, data, 16);
+}
+
+uint8_t PI3EQX12908::_read_reg(uint8_t mem_addr){
+  //Wire.beginTransmission(_I2C_ADDR);
+  //Wire.write(mem_addr);
+  //Wire.endTransmission();
+  Wire.requestFrom(_I2C_ADDR, (uint8_t)(mem_addr + 1));
+  for(uint8_t i=0; i<mem_addr; i++)
+    Wire.read();
+  return Wire.read();
+}
+
+void PI3EQX12908::_write_reg(uint8_t mem_addr, uint8_t value){
+  Wire.beginTransmission(_I2C_ADDR);
+  Wire.write(mem_addr);
+  Wire.write(value);
+  Wire.endTransmission();
+}
+
+void PI3EQX12908::_burst_read(uint8_t mem_addr, uint8_t* data, uint8_t len){
+  //Wire.beginTransmission(_I2C_ADDR);
+  //Wire.write(mem_addr);
+  //Wire.endTransmission();
+  Wire.requestFrom(_I2C_ADDR, (uint8_t)(mem_addr + len));
+  for(uint8_t i=0; i<mem_addr; i++)
+    Wire.read();
+  for(uint8_t i=0; i<len; i++)
+    data[i] = Wire.read();
+}
+
+void PI3EQX12908::_burst_write(uint8_t mem_addr, uint8_t* data, uint8_t len){
+  Wire.beginTransmission(_I2C_ADDR);
+  Wire.write(mem_addr);
+  for(uint8_t i=0; i<len; i++)
+    Wire.write(data[i]);
+  Wire.endTransmission();
+}
---
+++ PI3EQX12908A2.h
@@ -0,0 +1,223 @@
+#ifndef _PI3EQX12908_H
+#define _PI3EQX12908_H
+
+#include <stdint.h>
+#include <Wire.h>
+#include <String.h>
+
+#define SIGNAL_DETECT_REG   0
+#define RX_DETECT_REG       1
+#define POWER_DOWN_REG      2
+#define CONFIG_A0_REG       3
+#define CONFIG_A1_REG       4
+#define CONFIG_A2_REG       5
+#define CONFIG_A3_REG       6
+#define CONFIG_B0_REG       7
+#define CONFIG_B1_REG       8
+#define CONFIG_B2_REG       9
+#define CONFIG_B3_REG       10
+#define SIGNAL_DET_CFG_REG  11
+#define RX_DET_CFG_REG      12
+#define SIGNAL_DET_TH_REG   13
+
+#define CONFIG_A_OFFSET 3
+#define CONFIG_B_OFFSET 7
+
+#define A3_SHIFT  7
+#define A2_SHIFT  6
+#define A1_SHIFT  5
+#define A0_SHIFT  4
+#define B0_SHIFT  3
+#define B1_SHIFT  2
+#define B2_SHIFT  1
+#define B3_SHIFT  0
+
+#define EQ_SHIFT  4
+#define FG_SHIFT  2
+#define SW_SHIFT  0
+
+#define SDT_SHIFT 1
+
+#define FLAT_GAIN_M4db 0
+#define FLAT_GAIN_M2db 1
+#define FLAT_GAIN_00db 2
+#define FLAT_GAIN_P2db 3
+
+#define SWING_900mVpp   0
+#define SWING_1000mVpp  1
+
+#define SDT_OFF_30_ON_130_mVpp   0
+#define SDT_OFF_50_ON_150_mVpp   1
+#define SDT_OFF_70_ON_170_mVpp   2
+#define SDT_OFF_110_ON_210_mVpp  3
+
+#define CFG_ON  0
+#define CFG_OFF 1
+
+class PI3EQX12908{
+  public:
+    init(uint8_t i2c_addr);
+
+    // 0 - Signal Detect
+    uint8_t getSignalDetect();
+    uint8_t getSignalDetect_A();
+    uint8_t getSignalDetect_A(uint8_t index);
+    uint8_t getSignalDetect_B();
+    uint8_t getSignalDetect_B(uint8_t index);
+
+    // 1 - RX Detect
+    uint8_t getRxDetect();
+    uint8_t getRxDetect_A();
+    uint8_t getRxDetect_A(uint8_t index);
+    uint8_t getRxDetect_B();
+    uint8_t getRxDetect_B(uint8_t index);
+
+    // 2 - Power Down
+    uint8_t getPowerDown();
+    uint8_t getPowerDown_A();
+    uint8_t getPowerDown_A(uint8_t index);
+    uint8_t getPowerDown_B();
+    uint8_t getPowerDown_B(uint8_t index);
+    void setPowerDown(uint8_t isDown);
+    void setPowerDown_A(uint8_t isDown);
+    void setPowerDown_A(uint8_t index, uint8_t isDown);
+    void setPowerDown_B(uint8_t isDown);
+    void setPowerDown_B(uint8_t index, uint8_t isDown);
+
+    // 3 - A0 Config
+    uint8_t getConfig_A0();
+    uint8_t getEQ_A0();
+    uint8_t getFlatGain_A0();
+    uint8_t getSW_A0();
+    void setConfig_A0(uint8_t config);
+    void setEQ_A0(uint8_t EQ);
+    void setFlatGain_A0(uint8_t flat_gain);
+    void setSW_A0(uint8_t swing);
+
+    // 4 - A1 Config
+    uint8_t getConfig_A1();
+    uint8_t getEQ_A1();
+    uint8_t getFlatGain_A1();
+    uint8_t getSW_A1();
+    void setConfig_A1(uint8_t config);
+    void setEQ_A1(uint8_t EQ);
+    void setFlatGain_A1(uint8_t flat_gain);
+    void setSW_A1(uint8_t swing);
+
+    // 5 - A2 Config
+    uint8_t getConfig_A2();
+    uint8_t getEQ_A2();
+    uint8_t getFlatGain_A2();
+    uint8_t getSW_A2();
+    void setConfig_A2(uint8_t config);
+    void setEQ_A2(uint8_t EQ);
+    void setFlatGain_A2(uint8_t flat_gain);
+    void setSW_A2(uint8_t swing);
+
+    // 6 - A3 Config
+    uint8_t getConfig_A3();
+    uint8_t getEQ_A3();
+    uint8_t getFlatGain_A3();
+    uint8_t getSW_A3();
+    void setConfig_A3(uint8_t config);
+    void setEQ_A3(uint8_t EQ);
+    void setFlatGain_A3(uint8_t flat_gain);
+    void setSW_A3(uint8_t swing);
+
+    // 7 - B0 Config
+    uint8_t getConfig_B0();
+    uint8_t getEQ_B0();
+    uint8_t getFlatGain_B0();
+    uint8_t getSW_B0();
+    void setConfig_B0(uint8_t config);
+    void setEQ_B0(uint8_t EQ);
+    void setFlatGain_B0(uint8_t flat_gain);
+    void setSW_B0(uint8_t swing);
+
+    // 8 - B1 Config
+    uint8_t getConfig_B1();
+    uint8_t getEQ_B1();
+    uint8_t getFlatGain_B1();
+    uint8_t getSW_B1();
+    void setConfig_B1(uint8_t config);
+    void setEQ_B1(uint8_t EQ);
+    void setFlatGain_B1(uint8_t flat_gain);
+    void setSW_B1(uint8_t swing);
+
+    // 9 - B2 Config
+    uint8_t getConfig_B2();
+    uint8_t getEQ_B2();
+    uint8_t getFlatGain_B2();
+    uint8_t getSW_B2();
+    void setConfig_B2(uint8_t config);
+    void setEQ_B2(uint8_t EQ);
+    void setFlatGain_B2(uint8_t flat_gain);
+    void setSW_B2(uint8_t swing);
+
+    // 10 - B3 Config
+    uint8_t getConfig_B3();
+    uint8_t getEQ_B3();
+    uint8_t getFlatGain_B3();
+    uint8_t getSW_B3();
+    void setConfig_B3(uint8_t config);
+    void setEQ_B3(uint8_t EQ);
+    void setFlatGain_B3(uint8_t flat_gain);
+    void setSW_B3(uint8_t swing);
+
+    // 11 - Signal Detect Config
+    uint8_t getSignalDetectConfig();
+    uint8_t getSignalDetectConfig_A();
+    uint8_t getSignalDetectConfig_A(uint8_t index);
+    uint8_t getSignalDetectConfig_B();
+    uint8_t getSignalDetectConfig_B(uint8_t index);
+    void setSignalDetectConfig(uint8_t isDown);
+    void setSignalDetectConfig_A(uint8_t isDown);
+    void setSignalDetectConfig_A(uint8_t index, uint8_t isDown);
+    void setSignalDetectConfig_B(uint8_t isDown);
+    void setSignalDetectConfig_B(uint8_t index, uint8_t isDown);
+
+    // 12 - RX Detect Config
+    uint8_t getRxDetectConfig();
+    uint8_t getRxDetectConfig_A();
+    uint8_t getRxDetectConfig_A(uint8_t index);
+    uint8_t getRxDetectConfig_B();
+    uint8_t getRxDetectConfig_B(uint8_t index);
+    void setRxDetectConfig(uint8_t isDown);
+    void setRxDetectConfig_A(uint8_t isDown);
+    void setRxDetectConfig_A(uint8_t index, uint8_t isDown);
+    void setRxDetectConfig_B(uint8_t isDown);
+    void setRxDetectConfig_B(uint8_t index, uint8_t isDown);
+
+    // 13 - Signal Detect Threshold
+    uint8_t getSDTConfig();
+    uint8_t setSDTConfig(uint8_t thresh);
+
+    // Others
+    void setConfig_A(uint8_t config);
+    void setConfig_A(uint8_t index, uint8_t config);
+    void setConfig_B(uint8_t config);
+    void setConfig_B(uint8_t index, uint8_t config);
+    void setConfig(uint8_t config);
+    void setEQ_A(uint8_t EQ);
+    void setEQ_B(uint8_t EQ);
+    void setEQ(uint8_t EQ);
+    void setFG_A(uint8_t flat_gain);
+    void setFG_B(uint8_t flat_gain);
+    void setFG(uint8_t flat_gain);
+    void setSW_A(uint8_t swing);
+    void setSW_B(uint8_t swing);
+    void setSW(uint8_t swing);
+    void print_all();
+    void dump_all(uint8_t* data);
+
+  private:
+    uint8_t  _I2C_ADDR;
+    String _REGS[16];
+
+    uint8_t _read_reg(uint8_t mem_addr);
+    void _write_reg(uint8_t mem_addr, uint8_t value);
+    void _burst_read(uint8_t mem_addr, uint8_t* data, uint8_t len);
+    void _burst_write(uint8_t mem_addr, uint8_t* data, uint8_t len);
+};
+
+#endif
