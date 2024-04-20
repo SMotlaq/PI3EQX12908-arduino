3cba945c040d012512de42d48b561c2b5367063f
SMotlaq <pilot.motlaq@gmail.com>
2024-04-20 16:06:11 +0330

    feat(doc): description added to .cpp and .h files

--- PI3EQX12908A2.cpp
+++ PI3EQX12908A2.cpp
@@ -1,3 +1,24 @@
+/*!
+ * @file PI3EQX12908A2.cpp
+ *
+ * @mainpage PI3EQX12908 PCIe 3.0 redriver library
+ *
+ * @section intro_sec Introduction
+ *
+ * The PI3EQX12908 PCIe 3.0 redriver library is a software library that allows developers to easily access the I2C interfaces of the PI3EQX12908 PCIe 3.0 redriver chip.
+ * It provides a simple API for performing common I2C operations and is compatible with Arduino platform.
+ * The library includes example code to help developers get started quickly. 
+ *
+ * @section author Author
+ *
+ * Written by Salman Motlaq (@SMotlaq on GitHub)
+ *
+ * @section license License
+ *
+ * MIT License
+ *
+ */
+
 #include "PI3EQX12908A2.h"
 #include "Arduino.h"
 
