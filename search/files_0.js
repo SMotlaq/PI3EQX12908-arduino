1dd832498b87a2c5a7f27b0f4b730438fb2b82bd
SMotlaq <SMotlaq@users.noreply.github.com>
2024-04-29 10:00:28 +0000

    deploy: 7edfd88ab0c79bc08134dab477e3d6b805a17d16

--- search/files_0.js
+++ search/files_0.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['pi3eqx12908a2_2ecpp_0',['PI3EQX12908A2.cpp',['../a00002.html',1,'']]],
+  ['pi3eqx12908a2_2ecpp_0',['PI3EQX12908A2.cpp',['../a00008.html',1,'']]],
   ['pi3eqx12908a2_2eh_1',['PI3EQX12908A2.h',['../a00005.html',1,'']]]
 ];
--- search/searchdata.js
+++ search/searchdata.js
@@ -1,12 +1,30 @@
 var indexSectionsWithContent =
 {
+  0: "03acdfgiklprs",
+  1: "p",
+  2: "p",
+  3: "dgips",
+  4: "cfs",
+  5: "03lpr"
 };
 
 var indexSectionNames =
 {
+  0: "all",
+  1: "classes",
+  2: "files",
+  3: "functions",
+  4: "defines",
+  5: "pages"
 };
 
 var indexSectionLabels =
 {
+  0: "All",
+  1: "Classes",
+  2: "Files",
+  3: "Functions",
+  4: "Macros",
+  5: "Pages"
 };
 
